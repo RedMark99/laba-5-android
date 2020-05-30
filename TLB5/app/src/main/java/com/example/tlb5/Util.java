11ad2080a5e320055a5c7ac1bdd0aa2b8a59fb5b
RedMark99 <32640621+RedMark99@users.noreply.github.com>
2020-05-30 04:11:59 +0300

    5

---
+++ TLB5/app/src/main/java/com/example/tlb5/Util.java
@@ -0,0 +1,53 @@
+package com.example.tlb5;
+
+public class Util {
+
+    public static String hotel_title1 = "Best Western Plus Hotel Kronjylland";
+    public static String hotel_street1 = "Vestergade 53, 8900 Randers,";
+    public static String hotel_reit1 = "4";
+    public static String hotel_time1 = "Круглосуточно";
+
+    public static String hotel_title2 = "Best Western Hotel Europa";
+    public static String hotel_street2 = "H P Hanssens Gade 10, 6200 Aabenraa";
+    public static String hotel_reit2 = "2";
+    public static String hotel_time2 = "Круглосуточно";
+
+    public static String hotel_title3 = "Hotel Legoland";
+    public static String hotel_street3 = "Аstvej 10, 7190 Billund,";
+    public static String hotel_reit3 = "4";
+    public static String hotel_time3 = "Круглосуточно";
+
+    public static String rest_title1 = "Susters Arcade";
+    public static String rest_street1 = "117 N Wall St, Denmark";
+    public static String rest_reit1 = "5";
+    public static String rest_time1 = "03.00-09.00";
+
+    public static String rest_title2 = "Basil's II";
+    public static String rest_street2 = "5595 S County Road P, Denmark";
+    public static String rest_reit2 = "4.5";
+    public static String rest_time2 = "10.00-22.00";
+
+    public static String rest_title3 = "deGrand Family Restaurant";
+    public static String rest_street3 = "623 Depere Rd., Denmark";
+    public static String rest_reit3 = "4";
+    public static String rest_time3 = "06.00-21.00";
+
+    public static String museum_title1 = "Национальный музей Дании";
+    public static String museum_street1 = "Prince's Mansion, Ny Vestergade 10, 1471 København K";
+    public static String museum_reit1 = "4.5";
+    public static String museum_time1 = "10:00–17:00";
+
+    public static String museum_title2 = "Луизиана";
+    public static String museum_street2 = "Gl Strandvej 13, 3050 Humlebæk";
+    public static String museum_reit2 = "4.5";
+    public static String museum_time2 = "09:00–23:00";
+
+    public static String museum_title3 = "Designmuseum Denmark";
+    public static String museum_street3 = "Bredgade 68, 1260 København";
+    public static String museum_reit3 = "4.5";
+    public static String museum_time3 = "с 9:00 до 17:30";
+
+
+
+
+}
